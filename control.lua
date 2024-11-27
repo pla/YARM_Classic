fde010313726af810064658bfb0fb58b417fac4f
pla <massenpost@pla1.de>
2024-11-27 16:12:14 +0100

    Initial Commit

---
+++ control.lua
@@ -0,0 +1,26 @@
+require "resmon"
+require "remote"
+
+
+-- if this ever happens, I'll be enormously surprised
+if not resmon then error("YARM has become badly corrupted: the variable resmon should've been set!") end
+
+-- Enable Lua API global Variable Viewer
+-- https://mods.factorio.com/mod/gvv
+if script.active_mods["gvv"] then
+  require("__gvv__.gvv")()
+end
+
+script.on_init(resmon.init_globals)
+script.on_configuration_changed(resmon.init_globals)
+
+script.on_load(resmon.on_load)
+
+script.on_event(defines.events.on_player_created, resmon.on_player_created)
+script.on_event(defines.events.on_tick, resmon.on_tick)
+script.on_event(defines.events.on_gui_click, resmon.on_gui_click)
+script.on_event(defines.events.on_gui_closed, resmon.on_gui_closed)
+script.on_event("get-yarm-selector", resmon.on_get_selection_tool)
+script.on_event(defines.events.on_player_selected_area, resmon.on_player_selected_area)
+
+on_site_updated = script.generate_event_name()
---
+++ settings.lua
@@ -0,0 +1,94 @@
+data:extend({
+
+  -- Global settings
+  {
+    type = "int-setting",
+    name = "YARM-ticks-between-checks",
+    setting_type = "runtime-global",
+    order = "a",
+    default_value = 600,
+    minimum_value = 20,
+    maximum_value = 1200
+  },
+  {
+    type = "int-setting",
+    name = "YARM-entities-per-tick",
+    setting_type = "runtime-global",
+    order = "a",
+    default_value = 100,
+    minimum_value = 10,
+    maximum_value = 1000,
+  },
+  {
+    type = "bool-setting",
+    name = "YARM-map-markers",
+    setting_type = "runtime-global",
+    order = "b",
+    default_value = true,
+  },
+  {
+    type = "int-setting",
+    name = "YARM-overlay-step",
+    setting_type = "runtime-global",
+    order = "c",
+    default_value = 1,
+    minimum_value = 1,
+    maximum_value = 5
+  },
+  {
+    type = "bool-setting",
+    name = "YARM-site-prefix-with-surface",
+    setting_type = "runtime-global",
+    order = "d",
+    default_value = false
+  },
+  {
+    type = "bool-setting",
+    name = "YARM-debug-profiling",
+    setting_type = "runtime-global",
+    order = "zz[debug]",
+    default_value = false,
+  },
+
+  {
+    type = "bool-setting",
+    name = "YARM-adjust-for-productivity",
+    setting_type = "runtime-global",
+    order = "c",
+    default_value = false,
+  },
+  {
+    type = "bool-setting",
+    name = "YARM-productivity-show-raw-and-adjusted",
+    setting_type = "runtime-global",
+    order = "d",
+    default_value = false,
+  },
+  {
+    type = "string-setting",
+    name = "YARM-productivity-parentheses-part-is",
+    setting_type = "runtime-global",
+    order = "e",
+    default_value = "adjusted",
+    allowed_values = { "adjusted", "raw" }
+  },
+
+  -- Per user settings
+  {
+    type = "int-setting",
+    name = "YARM-warn-percent",
+    setting_type = "runtime-per-user",
+    order = "a",
+    default_value = 10,
+    minimum_value = 0,
+    maximum_value = 100
+  },
+  {
+    type = "string-setting",
+    name = "YARM-order-by",
+    setting_type = "runtime-per-user",
+    order = "b",
+    default_value = "percent-remaining",
+    allowed_values = { "alphabetical", "percent-remaining", "ore-type", "ore-count", "etd" }
+  },
+})
